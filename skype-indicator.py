012464b33f9eab8f154e6a360c9ca76d98b4b87e
Vadim Rutkovsky <roignac@gmail.com>
2011-02-08 18:16:17 +0200

    Reworked version of notifiers - now supports calls. TODO: Voicemail

--- skype-indicator.py
+++ skype-indicator.py
@@ -3,7 +3,7 @@
 #Copyright 2010 Jonathan Foucher
 #
 #Authors:
-#    Jonathan Foucher <jfoucher@6px.eu>
+#		Jonathan Foucher <jfoucher@6px.eu>
 #
 #This program is free software: you can redistribute it and/or modify it 
 #under the terms of either or both of the following licenses:
@@ -16,11 +16,11 @@
 #This program is distributed in the hope that it will be useful, but 
 #WITHOUT ANY WARRANTY; without even the implied warranties of 
 #MERCHANTABILITY, SATISFACTORY QUALITY or FITNESS FOR A PARTICULAR 
-#PURPOSE.  See the applicable version of the GNU Lesser General Public 
+#PURPOSE.	See the applicable version of the GNU Lesser General Public 
 #License for more details.
 #
 #You should have received a copy of both the GNU Lesser General Public 
-#License version 3 and version 2.1 along with this program.  If not, see 
+#License version 3 and version 2.1 along with this program.	If not, see 
 #<http://www.gnu.org/licenses/>
 #
 
@@ -33,245 +33,257 @@ import hashlib
 import Skype4Py
 import urllib
 import os
+import time
 
+class notification:
+	notif_type = None
+	handle = None
+	name = None
+	msg = None
+	count = 0
+	timestamp = None
+	indicator = None
+	avatar_path = None
+	avatar_file = None
+	pixbuf = None
+	skype = None
+	
+	def __init__(self, notif_type, handle, msg, skype):
+		self.notif_type = notif_type
+		self.handle = handle
+		self.skype = skype
+		self.msg = msg
+
+		# Determine a name for specific notif_type
+		self.name = {
+			'message': self.get_user_name,
+			'chat'	 : self.get_chat_name,
+			'call'	 : self.get_call_name
+		}.get(self.notif_type)(self.handle)
+
+		#Get a timestamp for missed event
+		self.timestamp = {
+			'message': self.get_timestamp_of_last_missed_message,
+			'chat'	 : self.get_timestamp_of_last_missed_message,
+			'call'	 : self.get_timestamp_of_last_missed_call
+		}.get(self.notif_type)(self.handle)
+
+		# Create an avatar
+		self.create_avatar(self.name)
+
+	def ellipsis(self, name):
+		""" Add an ellipsis after ellipsis_length symbols if name is too long """
+		name = (name[:ellipsis_length] + '..') if len(name) > ellipsis_length else name
+		return name
+
+	def get_user_name(self, handle):
+		"""getting user name"""
+		if self.skype.User(handle).DisplayName:
+			user_name = self.skype.User(handle).DisplayName
+		elif self.skype.User(handle).FullName:
+			user_name = self.skype.User(handle).FullName
+		else:
+			user_name = handle
+		return self.ellipsis(user_name)
+
+	def get_chat_name(self, handle):
+		"""getting chat name"""
+		return self.ellipsis(self.skype.Chat(handle).FriendlyName)
+
+	def get_call_name(self, handle):
+		"""getting call name - constructing it out of participants names"""
+		call_name = "Call from %s" % self.skype.Call(handle).PartnerDisplayName
+		return self.ellipsis(call_name)
+
+	def get_last_private_message(self, handle):
+		"""getting last private message """
+		return self.msg.Body
+
+	def get_last_chat_message(self, handle):
+		"""getting last message in chat"""
+		return "%s: %s" % (self.msg.FromDisplayName, self.msg.Body)
+
+	def get_call_notification_contents(self, handle):
+		"""getting call notification text"""
+		call = self.skype.Call(handle)
+		call_name = "Missed call with "
+		for participant in call.Participants:
+			call_name += "%s," % participant.DisplayName
+		return self.ellipsis(call_name)
+
+	def create_avatar(self, name):
+		""" create an avatar from avatar_data """
+		h=hashlib.md5(name).hexdigest()
+		self.avatar_file=os.path.join(avatar_directory, "%s.jpg" % str(h))
+		urllib.urlretrieve('http://friedcellcollective.net/monsterid/monster/%s/64' % h, self.avatar_file)
+		self.pixbuf=gtk.gdk.pixbuf_new_from_file(self.avatar_file)
+		return self.avatar_file
+
+	def get_timestamp_of_last_missed_message(self, handle):
+		"""getting a timestamp of last missed message"""
+		return self.msg.Timestamp
+
+	def get_timestamp_of_last_missed_call(self, handle):
+		""" getting timestamp of last missed call"""
+		return self.skype.Call(handle).Timestamp
+
+	def update_count(self):
+		"""update a count message for this indicator"""
+		self.count += 1
+		self.indicator.set_property("count", str(self.count))
+
+	def create_indicator(self):
+		""" Create a new active indicator """
+		#print "--- Creating an indicator entry: '%s', count: %s, timestamp: %s, avatar: %s" % (self.name, self.count, self.timestamp, self.avatar_path)
+		self.indicator = indicate.Indicator()
+		self.indicator.set_property("subtype", "im")
+		self.indicator.set_property("sender", self.name)
+		self.indicator.set_property_icon("icon", self.pixbuf)
+		self.indicator.set_property_time("time", self.timestamp)
+		self.indicator.set_property("draw-attention", "true")
+		self.indicator.show()
+		self.indicator.connect("user-display", self.display_skype)
+
+	def create_notification(self):
+		""" Create a new notification """
+		# Determine a message for specific notif_type
+		self.message_text = {
+			'message': self.get_last_private_message,
+			'chat'	 : self.get_last_chat_message,
+			'call'	 : self.get_call_notification_contents
+		}.get(self.notif_type)(self.handle)
+		n = pynotify.Notification(self.name, self.message_text, "notification-message-im")
+		n.set_property("icon-name", self.avatar_file)
+		n.show()
+
+	def display_skype(self, indicator, timestamp):
+		""" display skype window for specific notif_type """
+		#hide this indicator and make messaging menu go back to normal
+		self.indicator.hide()
+		self.indicator.set_property("draw-attention", "false")
+		#Call correct funtion to open the Skype window
+		{
+			'message': self.display_user,
+			'chat'	 : self.display_chat,
+			'call'	 : self.display_call
+		}.get(self.notif_type)(self.handle)
+		#Destroy the object by setting handle to None - will be removed during append_notification call
+		self.handle = None
+
+		#print "---Displaying skype window for %s with handle '%s'" % (self.notif_type, self.handle)
+
+	def display_user(self, handle):
+		self.skype.Client.OpenMessageDialog(handle)
+
+	def display_chat(self, handle):
+		self.skype.Client.OpenDialog('CHAT', handle)
+
+	def display_call(self, handle):
+		pass
+		#FIXME: Open call window
+		#self.skype.Client.OpenDialog('CALL', handle)
 
 class skypeIndicator:
-	notifShown={}
-	oldcount={}
-	count={}
-	indicator={}
+	server = None
+	notifications = []
 
 	def __init__(self):
-		print "init:"
-
+		""" Initialize """
 		#get skype control
-		self.skype= Skype4Py.Skype()
-		self.loadSkype()
+		self.skype = Skype4Py.Skype()
+		#Check that skype is running, otherwise - start it and wait for 10 secs
+		try:
+			if not self.skype.Client.IsRunning:
+				self.skype.Client.Start()
+		except:
+			os.system("skype &")
+			time.sleep(10)
+
+		try:
+			self.skype.Attach()
+		except Skype4Py.errors.SkypeAPIError:
+			print "Please open skype first"
+			self.noSkype()
 
 		#create notification icon
+		self.create_server()
+		#Setting event callbacks
+		self.skype.OnMessageStatus = self.callback_messages
+		self.skype.OnCallStatus = self.callback_calls
+		#Adding missed events
+		self.create_indicators()
+
+	def noSkype(self):
+		"""Shows a notification if skype is not started"""
+		n = pynotify.Notification('Start Skype', 'Please start skype otherwise this won\'t work', "notification-message-im")
+		n.set_property("icon-name",gtk.STOCK_DIALOG_WARNING)
+		n.show()
+		return n
+
+	def create_server(self):
+		""" Create a new Skype notification server """
+		#print "Registering a service"
+		#Create a notification server
 		self.server = indicate.indicate_server_ref_default()
 		self.server.set_type("message.im")
 		self.server.set_desktop_file("/usr/share/applications/skype.desktop")
 		self.server.connect("server-display", self.server_display)
 		self.server.show()
 
-		self.create_indicators()
-
-	def loadSkype(self):
-		print "loadSkype:"
-		#Check that skype is running, otherwise - start it and wait for 5 secs
-		try:
-			if not self.skype.Client.IsRunning:
-				self.skype.Client.Start()
-		except:
-			print "loadSkype: Starting up Skype"
-			os.system("skype &")
-			print "loadSkype: Waiting 10 seconds..."
-			time.sleep(10)
-
-		try:
-			self.skype.Attach()
-		except Skype4Py.errors.SkypeAPIError:
-			print "loadSkype: Can't attach to Skype"
-			print "Please open skype first"
-			self.noSkype()
-
-	def create_indicators(self):
-		"""Loads skype messages, displays them as notification bubbles and also shows them in the messaging menu"""
-		#Prepare a variable to store avatars
-		self.avatar_directory = os.path.expanduser("~/.cache/ubuntu-skype-indicator")
-		#Check that avatar dir exists, otherwise - create it
-		if not os.path.isdir(self.avatar_directory):
-			os.mkdir(self.avatar_directory)
-
-		#initialize count dictionaries
-		self.count={}
-		#get unread messages from skype, set self.unread variable
-		self.get_messages()
-
-		#self.unread is a dictionary having the username of the sender or chat name as key and a list of messages as value
-		for name in self.unread:
-			# Here we look at the first message from this user to set the messaging menu indicator
-			# we only want one indicator per user
-			msg=self.unread[name][0]
-
-			#initialize message count for this user
-			if name not in self.count:
-				self.count[name]=0
-			if name not in self.oldcount:
-				self.oldcount[name]=0
-
-			# if this user doesn't have his indicator yet
-			if name not in self.indicator:
-				# create indicator
-				self.indicator[name] = indicate.Indicator()
-				print "create_indicators: creating indicator for %s" % name
-
-				# Set indicator properties
-				self.fullname=self.name_from_handle(name)
-				self.indicator[name].set_property("subtype", "im")
-				self.indicator[name].set_property("sender", self.fullname )
-				self.indicator[name].set_property("handle", name)
-
-				#this gets the most user-friendly name available for this user
-				user=self.user_from_handle(name)
-
-				#Prepare a filename
-				self.file=os.path.join(self.avatar_directory, "%s.jpg" % self.fullname)
-
-				# get an avatar for this user/chat
-				try:
-					# This will only work on windows
-					user.SaveAvatarToFile(self.file)
-				except:
-					# So on linux we use a generated monster ID. Fun but useless!
-					h=hashlib.md5(name).hexdigest()
-					#TODO find a way to get skype avatars on linux
-					urllib.urlretrieve('http://friedcellcollective.net/monsterid/monster/%s/64' % h, self.file)
-
-				#convert the imge to a pixbuf
-				pixbuf=gtk.gdk.pixbuf_new_from_file(self.file)
-				# for use in the indicator
-				self.indicator[name].set_property_icon("icon", pixbuf)
-
-				# set the timestamp of the indicator (this is what makes the indicator display the time since the message was received
-				self.indicator[name].set_property_time("time", msg.Timestamp)
-
-				self.indicator[name].show()
-				# when the user clicks on the indicator message, open the skype messaging window for this user
-				self.indicator[name].connect("user-display", self.display_msg)
-				
-			msgbody = ''
-			#reverse list so latest message is at the bottom
-			for eachmsg in self.unread[name][::-1]:
-				# msgbody contains all the messages from that user so far
-				if hasattr(eachmsg,'Body'):
-					msgbody += eachmsg.Body + "\n"
-				else:
-					msgbody += "Missed call"
-
-			# We set this person's indicator body to the compound text
-			self.indicator[name].set_property("body", msgbody)
-
-			# if there are more than one message from this user, we set the indicator count to be displayed in the messaging menu.
-			# Otherwise the time elapsed since receiving the message will be shown
-			if self.count[name] > 1:
-				self.indicator[name].set_property("count", str(self.count[name]))
-			
-			# If a new message arrived since last time checked, mark notification as not shown
-			if self.count[name] > self.oldcount[name]:
-				self.notifShown[name]=False
-
-			#If notification marked as not shown, show it
-			if not self.notifShown.get(name, False) and self.showNotification(self.fullname, msgbody, self.file):
-				#mark notification as shown
-				self.notifShown[name]=True
-
-				self.indicator[name].set_property("draw-attention", "true")
-				self.indicator[name].show()
-				print "notification shown for", name
-
-			print "%d messages from %s" %(self.count[name],name)
-		# Set oldcountt variable for next loop
-		self.oldcount=self.count
-		# Loop runs as long as true is returned
-		return True
-
-
-	def name_from_handle(self,handle):
-		print "name_from_handle for %s" % handle
-		if '#' in handle:
-			return self.skype.Chat(handle).FriendlyName
-		else:
-			user=self.skype.User(handle)
-			if user.DisplayName:
-				return user.DisplayName
-			elif user.FullName:
-				return user.FullName
-			else:
-				return handle
-
-	def user_from_handle(self,handle):
-		print "user_from_handle for %s" % handle
-		if '#' in handle:
-			return self.skype.Chat(handle)
-		else:
-			return self.skype.User(handle)
-
-	def showNotification(self, title, message,file=None):
-		'''takes a title and a message to display the email notification. Returns the
-        created notification object'''
-
-		n = pynotify.Notification(title, message, "notification-message-im")
-		if file is not None:
-			n.set_property("icon-name",os.getcwd() + "/" + file)
-		n.show()
-
-		return n
-
-	def noSkype(self):
-		'''Shows a notification if skype is not started'''
-		title='Start Skype'
-		message='Please start skype otherwise this won\'t work'
-		n = self.showNotification(title, message)
-		n.set_property("icon-name",gtk.STOCK_DIALOG_WARNING)
-		n.show()
-
-		return n
-
-	def get_messages(self):
-		print "checking messages"
-		self.unread={}
-
-		for msg in self.skype.MissedMessages:
-			#Get number of people in chat
-			chat_members = len(msg.Chat.Members)
-			if (chat_members > 2):
-				#Its a chat
-				skype_name = msg.ChatName
-				#print("Adding message in chat '%s' (%s), members: %s" % (msg.Chat.FriendlyName, msg.ChatName, chat_members))
-			else:
-				#User message
-				skype_name = msg.FromHandle
-				#print("Adding message from '%s' (%s)" % (msg.FromDisplayName, msg.FromHandle))
-
-			if skype_name not in self.count:
-				self.count[skype_name]=0
-			if not skype_name in self.unread:
-				self.unread[skype_name]=[]
-			
-			self.unread[skype_name].append(msg)
-			self.count[skype_name]+=1
-
-		print "checking calls"
-		for call in self.skype.MissedCalls:
-			skype_name = call.PartnerHandle
-			if skype_name not in self.count:
-				self.count[skype_name]=0
-			if not skype_name in self.unread:
-				self.unread[skype_name]=[]
-			
-			self.unread[skype_name].append(call)
-			self.count[skype_name]+=1
-		return self.unread
-
 	def server_display(self, widget, timestamp=None):
-		#Show main Skype window
+		""" Open main Skype window """
 		self.skype.Client.Focus()
 
-	def display_msg(self, indicator, timestamp):
-		#hide this indicator
-		indicator.hide()
-		#messaging menu goes back to normal
-		indicator.set_property("draw-attention", "false")
-		# open the skype chat window for this user or chat
-		handle = indicator.get_property("handle")
-		if "#" in handle:
-			self.skype.Client.OpenDialog('CHAT', handle);
-		else:
-			self.skype.Client.OpenMessageDialog(indicator.get_property("handle"))
+	def create_indicators(self):
+		""" Check for missed messages / chats / calls """
+		for msg in self.skype.MissedMessages:
+			self.callback_messages(msg, 'RECEIVED')
+
+		for call in self.skype.MissedCalls:
+			self.callback_calls(self, call, 'RECEIVED')
+
+	def callback_messages(self, msg, status):
+		if status == 'RECEIVED':
+			chat_members = len(msg.Chat.Members)
+			if (chat_members > 2):
+				new_notification=notification("chat", msg.ChatName, msg, self.skype)
+			else:
+				new_notification=notification("message", msg.FromHandle, msg, self.skype)
+			self.append_notification(new_notification)
+
+	def callback_calls(self, call, status):
+		if status == Skype4Py.clsRinging and call.Type.startswith('INCOMING'):
+			new_notification=notification("call", call.Id, None, self.skype)
+			self.append_notification(new_notification)
+
+	def append_notification(self, new_notification):
+		""" Append a new notification, checking previous notifications"""
+		found = False
+		# Check that the indicator for this event type and handle has not already been created
+		for notification in self.notifications:
+			# Removing notifications with handle None
+			if notification.handle == None:
+				print "Removed notification"
+				del self.notifications[self.notifications.index(notification)]
+			if ((notification.handle == new_notification.handle) and (notification.notif_type == new_notification.notif_type)):
+				notification.update_count()
+				found = True
+
+		#Create a new indicator and notification
+		if not found:
+			new_notification.create_indicator()
+			new_notification.create_notification()
+			self.notifications.append(new_notification)
 
 if __name__ == "__main__":
+	#Global variables
+	avatar_directory = None
+	ellipsis_length = 30
+
+	#Prepare a variable to store avatars. Check that avatar dir exists, otherwise - create it
+	avatar_directory = os.path.expanduser("~/.cache/ubuntu-skype-indicator")
+	if not os.path.isdir(avatar_directory):
+		os.mkdir(avatar_directory)
 
 	skypeind=skypeIndicator()
 
